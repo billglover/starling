9333d7ef2508931009a5528a4183b5ac48b6b4ca
Bill Glover <bill@billglover.co.uk>
2018-03-25 10:41:01 +0100

    Test GetTransactions with no date range

--- transactions.go
+++ transactions.go
@@ -6,20 +6,29 @@ import (
 	"time"
 )
 
+// DateRange holds two dates that represent a range. It is typically
+// used when providing a range when querying the API.
+type DateRange struct {
+	From time.Time
+	To   time.Time
+}
+
 // GetTransactions returns a list of transaction summaries for the current user. It accepts optional
 // time.Time values to request transactions within a given date range. If these values are not provided
 // the API returns the last 100 transactions.
-func (c *Client) GetTransactions(ctx context.Context, from, to time.Time) (*Transactions, *http.Response, error) {
+func (c *Client) GetTransactions(ctx context.Context, dr *DateRange) (*Transactions, *http.Response, error) {
 
 	req, err := c.NewRequest("GET", "/api/v1/transactions", nil)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	q := req.URL.Query()
-	q.Add("from", from.Format("2006-01-02"))
-	q.Add("to", to.Format("2006-01-02"))
-	req.URL.RawQuery = q.Encode()
+	if dr != nil {
+		q := req.URL.Query()
+		q.Add("from", dr.From.Format("2006-01-02"))
+		q.Add("to", dr.To.Format("2006-01-02"))
+		req.URL.RawQuery = q.Encode()
+	}
 
 	var txns *Transactions
 	resp, err := c.Do(ctx, req, &txns)
