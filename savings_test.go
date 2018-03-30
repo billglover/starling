c20962fd701cd170adb472be1ecd68110af98ad3
Bill Glover <bill@billglover.co.uk>
2018-03-30 16:02:07 +0100

    Add descriptive logging to savings tests

--- savings_test.go
+++ savings_test.go
@@ -11,10 +11,13 @@ import (
 
 // TestGetSavingsGoals confirms that the client is able to query a list of savings goals.
 func TestGetSavingsGoals(t *testing.T) {
+
+	t.Log("Given the need to test fetching savings goals:")
+
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	json := `{
+	mock := `{
 		"savingsGoalList": [
 		  {
 			"uid": "e43d3060-2c83-4bb9-ac8c-c627b9c45f8b",
@@ -32,73 +35,41 @@ func TestGetSavingsGoals(t *testing.T) {
 		]
 	  }`
 
-	mux.HandleFunc("/api/v1/savings-goals", func(w http.ResponseWriter, r *http.Request) {
-		if got, want := r.Method, "GET"; got != want {
-			t.Errorf("request method: %v, want %v", got, want)
-		}
-		fmt.Fprint(w, json)
-	})
-
-	goals, _, err := client.GetSavingsGoals(context.Background())
-	if err != nil {
-		t.Error("unexpected error returned:", err)
-	}
-
-	want := &SavingsGoals{
-		SavingsGoalList: []SavingsGoal{
-			SavingsGoal{
-				UID:  "e43d3060-2c83-4bb9-ac8c-c627b9c45f8b",
-				Name: "Trip to Paris",
-				Target: CurrencyAndAmount{
-					Currency:   "GBP",
-					MinorUnits: 11223344,
-				},
-				TotalSaved: CurrencyAndAmount{
-					Currency:   "GBP",
-					MinorUnits: 11223344,
-				},
-				SavedPercentage: 50,
-			},
-		},
-	}
-
-	if !reflect.DeepEqual(goals, want) {
-		t.Errorf("GetSavingsGoals returned %+v, want %+v", goals, want)
-	}
-
-}
-
-// TestGetSavingsGoals_Error confirms that the client is able to parse a successful error response from the API.
-// It confirms that error messages are decoded from the JSON description and returned as the error string. It
-// also confirms that nil is returned for the savings goals in the event of an error.
-func TestGetSavingsGoals_Error(t *testing.T) {
-	client, mux, _, teardown := setup()
-	defer teardown()
-
-	mock := `{"Message":"this is an error message"}`
+	t.Log("\tWhen making a call to GetSavingsGoals():")
 
 	mux.HandleFunc("/api/v1/savings-goals", func(w http.ResponseWriter, r *http.Request) {
-		if got, want := r.Method, "GET"; got != want {
-			t.Errorf("request method: %v, want %v", got, want)
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, mock)
+		checkMethod(t, r, "GET")
+		fmt.Fprint(w, mock)
 	})
 
 	got, _, err := client.GetSavingsGoals(context.Background())
-	if err == nil {
-		t.Error("expected an error to be returned:")
+	if err != nil {
+		t.Fatal("\t\tshould be able to make the request", cross, err)
+	} else {
+		t.Log("\t\tshould be able to make the request", tick)
 	}
 
-	want := &ErrorDetail{}
+	t.Log("\tWhen parsing the response from the API:")
+
+	want := &SavingsGoals{}
 	json.Unmarshal([]byte(mock), want)
 
-	if err.Error() != want.Message {
-		t.Errorf("unexpected error message returned: %v", err)
+	if got == nil {
+		t.Fatal("\t\tshould not return 'nil'", cross)
+	} else {
+		t.Log("\t\tshould not return 'nil'", tick)
 	}
 
-	if got != nil {
-		t.Errorf("unexpected goals returned: %+v", got)
+	if !reflect.DeepEqual(got, want) {
+		t.Error("\t\tshould return a list matching the mock response", cross)
+	} else {
+		t.Log("\t\tshould return a transaction list matching the mock response", tick)
+	}
+
+	if len(got.SavingsGoalList) == 0 {
+		t.Errorf("\t\tshould have at least one transaction %s %d", cross, len(got.SavingsGoalList))
+	} else {
+		t.Log("\t\tshould have at least one transaction", tick)
 	}
 
 }
@@ -123,58 +94,42 @@ func TestGetSavingsGoal(t *testing.T) {
 		"savedPercentage": 50
 	}`
 
+	t.Log("\tWhen making a call to GetSavingsGoal():")
+
 	mux.HandleFunc("/api/v1/savings-goals/", func(w http.ResponseWriter, r *http.Request) {
-		if got, want := r.Method, "GET"; got != want {
-			t.Errorf("request method: %v, want %v", got, want)
-		}
+		checkMethod(t, r, "GET")
 		fmt.Fprint(w, mock)
 	})
 
 	// Define our request and execute the tests
 	got, _, err := client.GetSavingsGoal(context.Background(), "e43d3060-2c83-4bb9-ac8c-c627b9c45f8b")
 	if err != nil {
-		t.Error("unexpected error returned:", err)
+		t.Fatal("\t\tshould be able to make the request", cross, err)
+	} else {
+		t.Log("\t\tshould be able to make the request", tick)
 	}
+
+	t.Log("\tWhen parsing the response from the API:")
 
 	want := &SavingsGoal{}
 	json.Unmarshal([]byte(mock), want)
 
+	if got == nil {
+		t.Fatal("\t\tshould not return 'nil'", cross)
+	} else {
+		t.Log("\t\tshould not return 'nil'", tick)
+	}
+
 	if !reflect.DeepEqual(got, want) {
-		t.Errorf("GetSavingsGoal returned %+v, want %+v", got, want)
+		t.Error("\t\tshould return a list matching the mock response", cross)
+	} else {
+		t.Log("\t\tshould return a transaction list matching the mock response", tick)
 	}
 
-}
-
-// TestGetSavingsGoal_Error confirms that the client is able to parse a successful error response from the API.
-// It confirms that error messages are decoded from the JSON description and returned as the error string. It
-// also confirms that nil is returned for the savings goals in the event of an error.
-func TestGetSavingsGoal_Error(t *testing.T) {
-	client, mux, _, teardown := setup()
-	defer teardown()
-
-	msg := ErrorDetail{
-		Message: "this is an error message",
-	}
-
-	mux.HandleFunc("/api/v1/savings-goals/", func(w http.ResponseWriter, r *http.Request) {
-		if got, want := r.Method, "GET"; got != want {
-			t.Errorf("request method: %v, want %v", got, want)
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(msg)
-	})
-
-	goal, _, err := client.GetSavingsGoal(context.Background(), "e43d3060-2c83-4bb9-ac8c-c627b9c45f8b")
-	if err == nil {
-		t.Error("expected an error to be returned:")
-	}
-
-	if err.Error() != msg.Message {
-		t.Errorf("GetSavingsGoals returned '%v', want '%v'", err, msg.Message)
-	}
-
-	if goal != nil {
-		t.Errorf("unexpected goal returned: %+v", goal)
+	if len(got.UID) == 0 {
+		t.Errorf("\t\tshould have a UID %s %d", cross, len(got.UID))
+	} else {
+		t.Log("\t\tshould have a UID", tick)
 	}
 
 }
@@ -273,51 +228,6 @@ func TestPutSavingsGoal_ValidateError(t *testing.T) {
 	}
 }
 
-// TestPutSavingsGoal_ServerError confirms that the client is able to handle server errors when
-// making requests to the Savings Goal API.
-func TestPutSavingsGoal_ServerError(t *testing.T) {
-	client, mux, _, teardown := setup()
-	defer teardown()
-
-	// Define our mock response and handler
-	mock := `{"Message":"this is an error message"}`
-
-	mux.HandleFunc("/api/v1/savings-goals/", func(w http.ResponseWriter, r *http.Request) {
-		if got, want := r.Method, "PUT"; got != want {
-			t.Errorf("request method: %v, want %v", got, want)
-		}
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, mock)
-	})
-
-	// Define our request and execute the tests
-	sgr := SavingsGoalRequest{
-		Name:     "test",
-		Currency: "GBP",
-		Target: CurrencyAndAmount{
-			Currency:   "GBP",
-			MinorUnits: 10000,
-		},
-		Base64EncodedPhoto: "",
-	}
-
-	got, _, err := client.PutSavingsGoal(context.Background(), "d8770f9d-4ee9-4cc1-86e1-83c26bcfcc4f", sgr)
-	if err == nil {
-		t.Error("expected an error to be returned")
-	}
-
-	want := &ErrorDetail{}
-	json.Unmarshal([]byte(mock), want)
-
-	if err.Error() != want.Message {
-		t.Errorf("unexpected error message returned: %v", err)
-	}
-
-	if got != nil {
-		t.Errorf("unexpected goals returned: %+v", got)
-	}
-}
-
 // TestAddMoney confirms that the client is able to make a request to add money to a savings goal and parse
 // the successful response from the API.
 func TestAddMoney(t *testing.T) {
@@ -363,5 +273,13 @@ func TestAddMoney(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("AddMoney returned \n%+v, want \n%+v", got, want)
+	}
+}
+
+func checkMethod(t *testing.T, r *http.Request, want string) {
+	if got := r.Method; got != want {
+		t.Errorf("\t\tshould send a %s request to the API %s %s", want, cross, got)
+	} else {
+		t.Logf("\t\tshould send a %s request to the API %s", want, tick)
 	}
 }
