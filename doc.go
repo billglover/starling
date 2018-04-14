4e6fe1fa57fb4de10009ac1bcc2ca1aaa09c31cd
Bill Glover <bill@billglover.co.uk>
2018-04-14 13:42:04 +0100

    Add package documentation

---
+++ doc.go
@@ -0,0 +1,29 @@
+/*
+Package github provides a client for using the Starling API.
+
+Usage:
+
+	import "github.com/billglover/starling"
+
+Construct a new Starling client, then call various methods on the API to access
+different functions of the Starling API. For example:
+
+	client := starling.NewClient(nil)
+
+	// retrieve transactions for the current user
+	txns, _, err := client.Transactions(ctx, nil)
+
+The majority of the API calls will require you to pass in an access token:
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "TOKEN"},
+	)
+	ctx := context.Background()
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := starling.NewClient(tc)
+
+The Starling API documentation is available at https://developer.starlingbank.com/docs.
+
+*/
+package starling
