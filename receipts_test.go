7236a30a9f063baf38a64fc97fb1a71cc8c4f82c
Bill Glover <bill@billglover.co.uk>
2018-06-21 08:40:54 +0100

    Add test for panic on forbidden when requesting receipts

--- receipts_test.go
+++ receipts_test.go
@@ -91,3 +91,52 @@ func testCreateReceipt(t *testing.T, name string, rcpt Receipt, mock string, sta
 
 	checkStatus(t, resp, status)
 }
+
+func TestCreateReceiptForbidden(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v1/transactions/mastercard/", func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodPost)
+		w.WriteHeader(http.StatusForbidden)
+	})
+
+	receipt := Receipt{
+		UID:                "9af397e9-63e8-4a72-b3f6-4f0b068b2ed0",
+		EventUID:           "feada116-963e-4246-8fac-ed5e499b03d5",
+		MetadataSource:     "tests",
+		ReceiptIdentifier:  "0987654321",
+		MerchantIdentifier: "2b03a13a-bbfc-4479-8d4d-abb6a9119d27",
+		MerchantAddress:    "1 Merchant Way, Go Tests, UK",
+		TotalAmount:        1.23,
+		TotalTax:           0.45,
+		TaxReference:       "1234567890",
+		AuthCode:           "639682",
+		CardLast4:          "9012",
+		ProviderName:       "demo",
+		Items: []ReceiptItem{
+			{
+				UID:         "d74b17d5-f114-42d2-8adb-a6ddedc29298",
+				Description: "Large coffee",
+				Quantity:    2,
+				Amount:      12.34,
+				Tax:         1.23,
+				URL:         "https://crossenvsync",
+			},
+		},
+		Notes: []ReceiptNote{
+			{
+				UID:         "152dcc19-c86b-4f03-ba46-82aadbdcc957",
+				Description: "Large coffee",
+				URL:         "https://crossenvsync",
+			},
+		},
+	}
+
+	resp, err := client.CreateReceipt(context.Background(), "949404bd-d32e-4f1e-9759-4d6caee3137c", receipt)
+	checkHasError(t, err)
+
+	if resp.StatusCode != http.StatusForbidden {
+		t.Error("should return HTTP 403 status")
+	}
+}
