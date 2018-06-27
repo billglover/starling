8b77218e0fc23f4099cd58995f7226d97b2b157c
Bill Glover <bill@billglover.co.uk>
2018-06-27 08:10:54 +0100

    New: support for v2 accounts endpoint
    * Add function to request accounts
    * Add function to request Account Identifiers

--- accounts_test.go
+++ accounts_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path"
 	"reflect"
 	"testing"
 )
@@ -22,6 +23,26 @@ var accountsTC = []struct {
 				 		{
 							"accountUid": "24492cc9-77dd-4155-87a2-ec2580daf139",
 							"defaultCategory": "8d8c0f3b-f685-49ed-835e-db2ff8cef703",
+							"currency": "GBP",
+							"createdAt": "2017-05-24T07:43:46.664Z"
+						}
+					]
+				}`,
+	},
+	{
+		name:  "two personal accounts",
+		count: 2,
+		mock: `{
+					"accounts": [
+				 		{
+							"accountUid": "24492cc9-77dd-4155-87a2-ec2580daf139",
+							"defaultCategory": "8d8c0f3b-f685-49ed-835e-db2ff8cef703",
+							"currency": "GBP",
+							"createdAt": "2017-05-24T07:43:46.664Z"
+						},
+						{
+							"accountUid": "654BB6AB-3C10-49C2-9D4E-D49968772BB0",
+							"defaultCategory": "09e7e421-1afc-483a-98be-0b9da90f9a57",
 							"currency": "GBP",
 							"createdAt": "2017-05-24T07:43:46.664Z"
 						}
@@ -61,6 +82,106 @@ func testAccounts(t *testing.T, name string, count int, mock string) {
 		t.Error("should return an account matching the mock response")
 		t.Error(got)
 		t.Error(want)
+	}
+}
+
+func TestAccountsForbidden(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v2/accounts", func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusForbidden)
+	})
+
+	got, resp, err := client.Accounts(context.Background())
+	checkHasError(t, err)
+
+	if resp.StatusCode != http.StatusForbidden {
+		t.Error("should return HTTP 403 status")
+	}
+
+	if got != nil {
+		t.Error("should not return an account")
+	}
+}
+
+var accountIDsTC = []struct {
+	name string
+	uid  string
+	mock string
+}{
+	{
+		name: "personal account",
+		uid:  "2c7a379d-c0d8-4541-8520-ca41cc26d56a",
+		mock: `{
+			"accountIdentifier": "12345678",
+			"bankIdentifier": "608371",
+			"iban": "GB50SRLG60837112345678",
+			"bic": "SRLGGB2L"
+		 }`,
+	},
+}
+
+func TestAccountIDs(t *testing.T) {
+	for _, tc := range accountIDsTC {
+		t.Run(tc.name, func(st *testing.T) {
+			testAccountIDs(st, tc.name, tc.uid, tc.mock)
+		})
+	}
+}
+
+func testAccountIDs(t *testing.T, name, uid, mock string) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v2/accounts/", func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+
+		reqActUID := path.Base(path.Dir(r.URL.Path))
+		if reqActUID != uid {
+			t.Error("should send a request with the correct account UID")
+		}
+
+		resource := path.Base(r.URL.Path)
+		if resource != "identifiers" {
+			t.Error("should send a request for the identifiers resource")
+		}
+
+		fmt.Fprint(w, mock)
+	})
+
+	got, _, err := client.AccountID(context.Background(), uid)
+	checkNoError(t, err)
+
+	want := new(AccountID)
+	json.Unmarshal([]byte(mock), want)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Error("should return an AccountID matching the mock response")
+		t.Error(got)
+		t.Error(want)
+	}
+}
+
+func TestAccountIDsForbidden(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/v2/accounts/", func(w http.ResponseWriter, r *http.Request) {
+		checkMethod(t, r, http.MethodGet)
+		w.WriteHeader(http.StatusForbidden)
+	})
+
+	got, resp, err := client.AccountID(context.Background(), "2c7a379d-c0d8-4541-8520-ca41cc26d56a")
+	checkHasError(t, err)
+
+	if resp.StatusCode != http.StatusForbidden {
+		t.Error("should return HTTP 403 status")
+	}
+
+	if got != nil {
+		t.Error("should not return an account")
 	}
 }
 
