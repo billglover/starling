c379a8423871be05649c65c4c058ff2359844eb8
Bill Glover <bill@billglover.co.uk>
2018-06-29 09:21:36 +0100

    New: support for v2 feed endpoint
    * Define data types for the feed API
    * Add mock feed responses
    * Add function to retrieve feed items

--- feed.go
+++ feed.go
@@ -1,9 +1,13 @@
 package starling
 
-import "time"
+import (
+	"context"
+	"net/http"
+	"time"
+)
 
 // Feed is a slice of Items representing customer transactions
-type Feed struct {
+type feed struct {
 	Items []Item `json:"feedItems"`
 }
 
@@ -24,4 +28,19 @@ type Item struct {
 	Reference                string    `json:"reference"`
 	Country                  string    `json:"country"`
 	SpendingCategory         string    `json:"spendingCategory"`
+}
+
+// Feed returns a slice of Items for a given account and category
+func (c *Client) Feed(ctx context.Context, act, cat string) ([]Item, *http.Response, error) {
+	req, err := c.NewRequest("GET", "/api/v2/feed/account/"+act+"/category/"+cat, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var f feed
+	resp, err := c.Do(ctx, req, &f)
+	if err != nil {
+		return nil, resp, err
+	}
+	return f.Items, resp, nil
 }
