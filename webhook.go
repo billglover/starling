191b5c53b9686ad06ef5476e7dc522206ec638b5
Colin Seymour <lildood@gmail.com>
2019-01-11 17:27:37 +0000

    Update fields to remove deprecated fields

--- webhook.go
+++ webhook.go
@@ -12,26 +12,20 @@ import (
 
 // WebHookPayload defines the structure of the Starling web hook payload
 type WebHookPayload struct {
-	WebhookNotificationUID string         `json:"webhookNotificationUid"`
-	Timestamp              time.Time      `json:"timestamp"`
-	Content                WebHookContent `json:"content"`
-	AccountHolderUID       string         `json:"accountHolderUid"`
-	WebhookType            string         `json:"webhookType"`
-	CustomerUID            string         `json:"customerUid"`
-	UID                    string         `json:"uid"`
+	WebhookNotificationUID string            `json:"webhookNotificationUid"`
+	TransactionTimestamp   time.Time         `json:"transactionTimestamp"`
+	TransactionAmount      CurrencyAndAmount `json:"transactionAmount"`
+	SourceAmount           CurrencyAndAmount `json:"sourceAmount"`
+	Description            string            `json:"description"`
+	AccountHolderUID       string            `json:"accountHolderUid"`
+	WebhookType            string            `json:"webhookType"`
+	CustomerUID            string            `json:"customerUid"`
 }
 
-// WebHookContent defines the structure of the Starling web hook content
-type WebHookContent struct {
-	Class          string  `json:"class"`
-	TransactionUID string  `json:"transactionUid"`
-	Amount         float64 `json:"amount"`
-	SourceCurrency string  `json:"sourceCurrency"`
-	SourceAmount   float64 `json:"sourceAmount"`
-	CounterParty   string  `json:"counterParty"`
-	Reference      string  `json:"reference"`
-	Type           string  `json:"type"`
-	ForCustomer    string  `json:"forCustomer"`
+// CurrencyAndAmount defined the structure of the transaction and sournce amount contents
+type CurrencyAndAmount struct {
+	Currency   string `json:"currency"`
+	MinorUnits int64  `json:"minorUnits"`
 }
 
 // Validate takes an http request and a web-hook secret and validates the
